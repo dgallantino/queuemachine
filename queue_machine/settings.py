82fe182c3ab8916cd579ffd3c906319afbbb1d01
dgallantino <dimas.galant@gmail.com>
2018-08-26 21:25:01 +0700

    forgot to branch out

--- queue_machine/settings.py
+++ queue_machine/settings.py
@@ -33,14 +33,13 @@ LOGIN_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
+	'queue_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'queue_app',
-
 ]
 
 MIDDLEWARE = [
