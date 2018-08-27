9d018854271cbf32f0f0d96ec43291aba0ed70db
dgallantino <dimas.galant@gmail.com>
2018-08-27 12:02:22 +0700

    adding logout page

--- queue_machine/settings.py
+++ queue_machine/settings.py
@@ -34,12 +34,12 @@ LOGIN_REDIRECT_URL = '/'
 
 INSTALLED_APPS = [
 	'queue_app',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
