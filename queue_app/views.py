bd93a2af2204fbab85a28544d8eba51b609b1411
dgallantino <dimas.galant@gmail.com>
2018-09-03 11:43:09 +0700

    feature addment preparation fixes

--- queue_app/views.py
+++ queue_app/views.py
@@ -1,12 +1,12 @@
 from queue_app.models import Service, Queue
-from queue_app.serializers import QueueSerializer, SingleServiceSerializer
+from queue_app.serializers import QueueSerializer
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.forms.models import model_to_dict
 from django.views.generic import ListView, DetailView
 from django.http import JsonResponse
 
-class IndexView(ListView):
+class IndexView(LoginRequiredMixin, ListView):
 	login_url = '/accounts/login/'
 	template_name ='queue_app/index.html'
 	model = Service
@@ -18,10 +18,10 @@ class PrintTicket(DetailView):
 	http_method_names = ['post','get']
 	object_name = 'queue';
 	def add_next_queue(self, service):
-		current_queue = service.queues.last()
-		if current_queue:
+		last_queue = service.queues.last()
+		if last_queue:
 			next_queue = service.queues.create(
-				number = current_queue.number + 1
+				number = last_queue.number + 1
 				)
 			return next_queue
 		next_queue = service.queues.create(number = 1)	
@@ -31,7 +31,6 @@ class PrintTicket(DetailView):
 		return self.add_next_queue(super().get_object())
 	def render_to_response(self, context,**response_kwargs):
 		response = super().render_to_response(context, **response_kwargs)
-		context_data = model_to_dict(response.context_data[self.object_name])
 		return JsonResponse(QueueSerializer(self.object).data)
 
 '''Function Based Approach'''
@@ -43,7 +42,7 @@ def add_next_queue(p_service):
 		next_queue = Queue(service = p_service, number = 1)
 	next_queue.save()
 	return next_queue
-# @login_required
+@login_required
 def print_ticket(request, service_id):
 	if request.user.is_authenticated:
 		try:
