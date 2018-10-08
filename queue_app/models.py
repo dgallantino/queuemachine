4c7df7927722d93fe554b3afb61a1806f56f1f35
dgallantino <dimas.galant@gmail.com>
2018-10-08 12:37:59 +0700

    add feature booking : model and migrations

--- queue_app/models.py
+++ queue_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import date
 import uuid
+from django.contrib.auth.models import User
 
 class ServiceQueryset(models.QuerySet):
 	"""docstring for ServiceQueryset."""
@@ -46,8 +47,14 @@ class Queue(models.Model):
 		null=True,
 		blank=True,
 		related_name='queues')
+	customer=models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		null=True,
+		blank=True,
+		related_name='queues')
 	id=models.UUIDField(
-		primary_key=True, 
+		primary_key=True,
 		default=uuid.uuid4, 
 		editable=False)
 	number=models.IntegerField(
@@ -55,6 +62,8 @@ class Queue(models.Model):
 		blank=True)
 	date_created=models.DateTimeField(auto_now_add=True)
 	date_modified=models.DateTimeField(auto_now=True)
+	booking_flag=models.BooleanField(default=False)
+	booking_time=models.DateTimeField(null=True,blank=True,)
 	call_flag=models.BooleanField(default=False)
 	objects=QueueQueryset.as_manager()
 	def __str__(self):
