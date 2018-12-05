bfc12d6ca266bbe7ee5559c3b7b9e09141b44b76
dgallantino <dimas.galant@gmail.com>
2018-12-05 16:27:57 +0700

    change models.Organization

--- queue_app/models.py
+++ queue_app/models.py
@@ -2,38 +2,37 @@ from django.db import models
 from datetime import date
 from django.contrib.auth.models import AbstractUser, Group
 import uuid
-	
+
 #todos:
 #add customer profile
 
+
+
 # Organization will be deprocated
 # Using Group instead
-class Organization(models.Model):
+class Organization(Group):
 	id=models.UUIDField(
 		primary_key=True,
 		default=uuid.uuid4,
 		editable=False,
 	)
 	date_created=models.DateTimeField(auto_now_add=True)
-	
+
 	date_modified=models.DateTimeField(auto_now=True)
-	
+
 	name=models.CharField(max_length = 20)
-	
-	desc=models.CharField(max_length = 20)
-	
-	
+
 	def __str__(self):
 		return self.name
-	
+
 class CounterBooth(models.Model):
-	
+
 	id=models.UUIDField(
 		primary_key=True,
 		default=uuid.uuid4,
 		editable=False,
 	)
-	
+
 	groups=models.ForeignKey(
 		Group,
 		blank=True,
@@ -41,17 +40,17 @@ class CounterBooth(models.Model):
 		on_delete=models.CASCADE,
 		null=True,
 	)
-	
+
 	display_name=models.CharField(max_length = 20)
-	
+
 	spoken_name=models.CharField(max_length = 30)
-	
+
 	desc=models.CharField(max_length = 200)
-	
+
 	date_created=models.DateTimeField(auto_now_add=True)
-	
+
 	date_modified=models.DateTimeField(auto_now=True)
-	
+
 	organization=models.ForeignKey(
 		Organization,
 		on_delete=models.CASCADE,
@@ -59,10 +58,10 @@ class CounterBooth(models.Model):
 		null=True,
 		blank=False,
 	)
-	
+
 	def __str__(self):
 		return self.display_name
-	
+
 	def to_flat_dict(self):
 		return {
 			'id' : str(self.id),
@@ -71,89 +70,93 @@ class CounterBooth(models.Model):
 			'desc' : self.desc,
 			'group': str(self.groups.id),
  		}
-	
+
 class User(AbstractUser):
 	id=models.UUIDField(
 		primary_key=True,
 		default=uuid.uuid4,
 		editable=False
 	)
-	
-	organization=models.ForeignKey(
+
+	organization=models.ManyToManyField(
 		Organization,
 		on_delete=models.CASCADE,
 		related_name='users',
 		null=True,
 		blank=True,
 	)
-	
+
 	phone=models.CharField(
 		null=True,
 		blank=True,
 		max_length = 12,
 	)
-	
+
 	def __str__(self):
 		return self.username
-	
+
 # class CustomerProfile(models.Model):
 # 	id=models.UUIDField(
 # 		primary_key=True,
 # 		default=uuid.uuid4,
 # 		editable=False
 # 	)
-# 	
+#
 # 	user=models.OneToOneField(
 # 		User,
 # 		on_delete=models.CASCADE,
 # 		related_name='customer_profile'
 # 	)
-# 	
+#
 # 	groups=models.ManyToManyField(
 # 		Group,
 # 		blank=True,
 # 		related_name='customers',
 # 	)
-# 	
+#
 # 	birth_modified=models.DateTimeField(null=True,blank=True)
-# 
+#
 # 	first_name = models.CharField(max_length=20)
-# 	
+#
 # 	last_name = models.CharField(max_length=20)
-# 	
+#
 # 	email = models.EmailField()
-# 	
+#
 # 	address = models.CharField(max_length=200)
-# 	
+#
 # 	phone=models.CharField(
 # 		null=True,
 # 		blank=True,
 # 		max_length = 12,
 # 	)
-# 	
+#
 # 	date_created=models.DateTimeField(auto_now_add=True)
-# 	
-# 	date_modified=models.DateTimeField(auto_now=True)	
+#
+# 	date_modified=models.DateTimeField(auto_now=True)
 
 class ServiceQueryset(models.QuerySet):
 	"""docstring for ServiceQueryset."""
 	def get_last_queue(self):
 		return self.queues.last()
+
 	def group_filter(self, iterable):
 		return self.filter(groups__in=iterable)
 
+	def org_filter(self, org):
+		return self.filter(organization=org)
+
 class Service(models.Model):
-	
+
 	id=models.UUIDField(
-		primary_key=True, 
-		default=uuid.uuid4, 
+		primary_key=True,
+		default=uuid.uuid4,
 		editable=False,
 	)
-	
+
 	date_created=models.DateTimeField(auto_now_add=True)
-	
+
 	date_modified=models.DateTimeField(auto_now=True)
-	
+
 	organization=models.ForeignKey(
 		Organization,
 		on_delete=models.CASCADE,
@@ -161,25 +164,25 @@ class Service(models.Model):
 		null=True,
 		blank=False,
 	)
-	
+
 	groups=models.ManyToManyField(
 		Group,
 		blank=True,
 		related_name='services',
 	)
-	
+
 	name=models.CharField(max_length = 30)
-	
+
 	desc=models.CharField(max_length = 200)
-	
+
 	queue_char=models.CharField(
 		null=True,
 		blank=True,
 		max_length=1,
 	)
-	
+
 	objects=ServiceQueryset.as_manager()
-	
+
 	def __str__(self):
 		return self.name
 
@@ -187,53 +190,53 @@ class Service(models.Model):
 class QueueQueryset(models.QuerySet):
 	def today_filter(self):
 		return self.filter(date_created__date=date.today())
-	
+
 	def is_printed(self, flag=None):
 		if flag is not None:
 			return self.filter(is_printed=flag)
 		return self
-	
+
 	def is_booking(self, flag=None):
 		if flag is not None:
 			return self.filter(is_booking=flag)
 		return self
-	
+
 	def is_called(self, flag=None):
 		if flag is not None:
 			return self.filter(is_called=flag)
 		return self
-	
+
 	def services_filter(self, iterable):
 		return self.filter(service__in=iterable)
 
 class Queue(models.Model):
 	id=models.UUIDField(
 		primary_key=True,
-		default=uuid.uuid4, 
+		default=uuid.uuid4,
 		editable=False
 	)
-	
+
 	date_created=models.DateTimeField(auto_now_add=True)
-	
+
 	date_modified=models.DateTimeField(auto_now=True)
-	
+
 	service=models.ForeignKey(
 		Service,
 		on_delete=models.CASCADE,
 		related_name='queues'
 	)#
-	
+
 	character=models.CharField(
 		null=True,
 		blank=True,
 		max_length=1,
 	)
-	
+
 	number=models.IntegerField(
 		null=True,
 		blank=True
 	)
-	
+
 	customer=models.ForeignKey(
 		User,
 		on_delete=models.CASCADE,
@@ -241,7 +244,7 @@ class Queue(models.Model):
 		blank=True,
 		related_name='queues'
 	)#
-	
+
 	counter_booth=models.ForeignKey(
 		CounterBooth,
 		on_delete=models.SET_NULL,
@@ -249,26 +252,25 @@ class Queue(models.Model):
 		blank=True,
 		related_name='queues'
 	)#
-	
+
 	is_booking=models.BooleanField(default=False)#
-		
+
 	is_called=models.BooleanField(default=False)#
-	
+
 	is_printed=models.BooleanField(default=False)#
-	
+
 	booking_datetime=models.DateTimeField(null=True,blank=True,)
-	
+
 	print_datetime=models.DateTimeField(null=True, blank=True)
-	
+
 	objects=QueueQueryset.as_manager()
-	
+
 	def __str__(self):
 		return self.service.name+'/'+str(self.number)+'/'+str(self.pk)
-	
+
 	def save(self, *args, **kwargs):
 		self.character = self.character or self.service.queue_char
 		super().save(*args, **kwargs)
-	
+
 	class Meta:
 		ordering=['date_created']
-		
