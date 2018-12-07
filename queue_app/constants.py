25fa8c0512fd42d0e7ca32a4b97c764fb84ed0e1
dgallantino <dimas.galant@gmail.com>
2018-12-07 12:30:33 +0700

    adding constants

---
+++ queue_app/constants.py
@@ -0,0 +1,9 @@
+class IDX(object):
+    ORG = 'Organization'
+    BOOTH = 'CounterBooth'
+
+class TEMPLATE_OBJ_NAMES(object):
+    QUEUES = 'Queues'
+    QUEUE = 'Queue'
+    SERVICES = 'Services'
+    SERVICE = 'Service'
