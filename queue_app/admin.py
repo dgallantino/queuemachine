ba0c1b4d0d93ca2b417fe556c071f74d29e72447
dgallantino <dimas.galant@gmail.com>
2018-12-10 17:04:34 +0700

    admin fixin

--- queue_app/admin.py
+++ queue_app/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext as _
+
+
 # Register your models here.
 from queue_app import models, forms
-from django.contrib.auth.admin import UserAdmin 
+
+admin.site.unregister(Group)
 
 class QueueAdmin(admin.ModelAdmin):
     form = forms.QueueModelBaseForms
     model = models.Queue
     list_display = [ 'id', 'service','number','is_booking','is_called',]
     list_filter = ('service','is_booking', 'is_printed','is_called')
+    date_hierarchy = 'date_created'
     readonly_fields = (
                     'date_created',
                     'date_modified',
@@ -46,27 +53,33 @@ class CustomUserAdmin(UserAdmin):
     add_form = forms.CustomUserCreationForm
     form = forms.CustomUserChangeForm
     model = models.User
-    list_display = [ 'username', 'first_name','last_name',]
+    list_display = ('username', 'first_name','last_name',)
+    list_filter = UserAdmin.list_filter+('organization',)
+    filter_horizontal = UserAdmin.filter_horizontal + ('organization',)
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('username', ('first_name','last_name',),'password1', 'password2'),
         }),
     )
-    fieldsets = UserAdmin.fieldsets + (
-            ('Profile', {
-                'fields': (
-                    'organization','phone'
-                )
-            }
-        ),
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': (('first_name', 'last_name',),
+                                        'email', 'phone',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'organization','groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     actions = ['set_selected_as_staff',]
     def set_selected_as_staff(self,request,query_set):
         query_set.update(is_staff = True)
 
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (_('name'), _('organization'),)
+    list_filter = ('organization',)
+
 admin.site.register(models.User, CustomUserAdmin)
-admin.site.register(models.Service)
+admin.site.register(models.Service, ServiceAdmin)
 admin.site.register(models.Queue,QueueAdmin)
 admin.site.register(models.Organization)
 admin.site.register(models.CounterBooth)
